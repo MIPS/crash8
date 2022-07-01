8773f57b0a060a8181faa04afeaf9526f9fbf414
Dimitrije Milosevic <dimitrije.milosevic@syrmia.com>
2022-07-01 11:05:29 +0200

    extensions: Add MIPS64 support for gcore.

--- extensions/gcore.mk
+++ extensions/gcore.mk
@@ -18,6 +18,8 @@ PERIOD=2010, 2011, 2012, 2013, 2014, 2016, 2017, 2018, 2019, 2020, 2021
 
 ARCH=UNSUPPORTED
 
+ifeq (,$(TARGET))
+
 ifeq ($(shell arch), i686)
   TARGET=X86
   TARGET_CFLAGS=-D_FILE_OFFSET_BITS=64
@@ -48,6 +50,12 @@ ifeq ($(shell arch), mips)
   ARCH=SUPPORTED
 endif
 
+ifeq ($(shell arch), mips64)
+  TARGET=MIPS64
+  TARGET_CFLAGS=
+  ARCH=SUPPORTED
+endif
+
 ifeq ($(shell arch), ppc64)
   TARGET=PPC64
   TARGET_CFLAGS=
@@ -58,6 +66,13 @@ ifeq ($(shell arch), ppc64le)
   TARGET=PPC64
   TARGET_CFLAGS=
   ARCH=SUPPORTED
+endif
+
+else
+
+  TARGET_CFLAGS=
+  ARCH=SUPPORTED
+
 endif
 
 ifeq ($(shell /bin/ls /usr/include/crash/defs.h 2>/dev/null), /usr/include/crash/defs.h)
@@ -93,6 +108,10 @@ endif
 
 ifneq (,$(findstring $(TARGET), MIPS))
 GCORE_CFILES += libgcore/gcore_mips.c
+endif
+
+ifneq (,$(findstring $(TARGET), MIPS64))
+GCORE_CFILES += libgcore/gcore_mips64.c
 endif
 
 ifneq (,$(findstring $(TARGET), PPC64))
